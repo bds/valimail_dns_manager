a31371af0478d60035c011cf5399160e69acc8dc
Brian D. Smith <brian@talkdesk.com>
2017-11-14 15:48:01 -0800

    Add SQLite3

--- valimail_dns_manager/config/environments/production.rb
+++ valimail_dns_manager/config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.database_url = "db/production.sqlite3"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
